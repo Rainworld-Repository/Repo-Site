aa842e07bac261b0f01ce283f1e8f0fcc1c6f353
MagicaJaphet <Linda.Shutter@gmail.com>
2025-11-25 00:40:59 -0500

    Update zips to include jsons instead of fetching git API

--- assets/js/include_packs.js
+++ assets/js/include_packs.js
@@ -1,12 +1,9 @@
 const BASE_URL = window.location
 
 async function fetchFiles(path) {
-  const url = `https://api.github.com/repos/Rainworld-Repository/Repo-Site/contents/${path}`;
-
   try {
-    const response = await fetch(url, {
+    const response = await fetch(`./${path}/files.json`, {
       headers: {
-        'Authorization': `Bearer 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`,
         'Accept': 'application/vnd.github.v3+json' // Specify API version
       }
     });
